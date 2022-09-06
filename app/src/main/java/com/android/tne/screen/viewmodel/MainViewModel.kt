1355f1bfade7ccbe7e7df0073accbde49042194f
mjh2550 <moomjh94@naver.com>
2022-09-06 11:07:43 +0900

    Data Binding setting

--- app/src/main/java/com/android/tne/screen/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/android/tne/screen/viewmodel/MainViewModel.kt
@@ -1,5 +1,8 @@
 package com.android.tne.screen.viewmodel
 
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.android.tne.base.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
@@ -7,7 +10,6 @@ import javax.inject.Inject
 /**
  * Created by KimBH on 2022/07/12.
  */
-//@HiltViewModel
-class MainViewModel (
-) : BaseViewModel() {
+@HiltViewModel
+class MainViewModel @Inject constructor () : BaseViewModel() {
 }
