5bc0a84e7c333d92a8a9dbefb7a46e5a63c91466
mjh2550 <moomjh94@naver.com>
2022-09-06 14:39:22 +0900

    loadingDialog Setting

--- app/src/main/java/com/android/tne/network/loading/LoadingDialog.kt
+++ app/src/main/java/com/android/tne/network/loading/LoadingDialog.kt
@@ -2,7 +2,9 @@ package com.android.tne.network.loading
 
 import android.app.Dialog
 import android.content.Context
+import android.view.Window
 import android.view.WindowManager
+import com.android.tne.R
 
 /**
  * Created by KimBH on 2022/07/12.
@@ -27,8 +29,10 @@ class LoadingDialog {
         if (context != null) {
             mLoadingDialog = Dialog(context)
             mLoadingDialog.window?.clearFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND)
-
             // 로딩화면 디자인..
+            mLoadingDialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+            mLoadingDialog.setContentView(R.layout.progressdialog)
+            mLoadingDialog.setCancelable(false)
 
             mLoadingDialog.show()
         }
--- app/src/main/java/com/android/tne/screen/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/android/tne/screen/viewmodel/MainViewModel.kt
@@ -3,8 +3,12 @@ package com.android.tne.screen.viewmodel
 import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
 import com.android.tne.base.BaseViewModel
+import com.android.tne.base.response.ResponseResult
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 /**
@@ -12,4 +16,19 @@ import javax.inject.Inject
  */
 @HiltViewModel
 class MainViewModel @Inject constructor () : BaseViewModel() {
+    val testStr = MutableLiveData("testLiveStr")
+
+    fun click(){
+        testStr.value = "clicked"
+        viewModelScope.launch {
+
+            isLoading.value = true
+
+            delay(5000)
+
+            isLoading.value = false
+        }
+
+
+    }
 }
